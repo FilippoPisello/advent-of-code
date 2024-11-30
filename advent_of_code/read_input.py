ab6689693b30ee7c17c86efb51db002280bce65e
Filippo Pisello <66630817+FilippoPisello@users.noreply.github.com>
2024-11-30 18:33:06 +0100

    Introduce years (#6)
    * Introduce years
    
    * Fix missing references

--- advent_of_code/read_input.py
+++ advent_of_code/read_input.py
@@ -3,12 +3,12 @@ from pathlib import Path
 ROOT = Path(__file__).parent.resolve()
 
 
-def read_txt_input(day_number: int, user: str) -> list[str]:
+def read_txt_input(day_number: int, user: str, year: int) -> list[str]:
     """Reads the input file for the given day number.
 
     Each line is an element of the returned list.
     """
-    input_file = ROOT / f"day{day_number}/{user}/input.txt"
+    input_file = ROOT / f"{year}/day{day_number}/{user}/input.txt"
     content = input_file.read_text()
 
     ## uncomment for days 1 to 4 and later
