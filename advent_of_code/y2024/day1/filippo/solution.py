11dbad5aa19fb336e9c5407f9a757964f1339afc
FilippoPisello <filippo.pisello@live.it>
2024-12-01 11:57:56 +0100

    Filippo, day 1, part 1

---
+++ advent_of_code/y2024/day1/filippo/solution.py
@@ -0,0 +1,24 @@
+"""Solution for day 1 of Advent of Code 2024, by filippo."""
+
+from typing import Any
+
+
+def main_part_one(problem_input: str) -> Any:
+    left_list, right_list = [], []
+    for line in problem_input.splitlines():
+        left, right = line.split("   ")
+        left_list.append(int(left))
+        right_list.append(int(right))
+    sorted_left_list = sorted(left_list)
+    sorted_right_list = sorted(right_list)
+    return sum(
+        abs(left - right) for left, right in zip(sorted_left_list, sorted_right_list)
+    )
+
+
+def main_part_two(problem_input: str) -> Any:
+    return
+
+
+# def main(problem_input: str) -> Any:
+#    return
