23c7ad79a361dc90114c8904035e7be74808d307
FilippoPisello <filippo.pisello@live.it>
2024-12-01 12:05:43 +0100

    Filippo, day 1, part 2

--- advent_of_code/y2024/day1/filippo/solution.py
+++ advent_of_code/y2024/day1/filippo/solution.py
@@ -1,21 +1,36 @@
 """Solution for day 1 of Advent of Code 2024, by filippo."""
 
+from collections import Counter
 from typing import Any
 
 
 def main_part_one(problem_input: str) -> Any:
-    left_list, right_list = [], []
-    for line in problem_input.splitlines():
-        left, right = line.split("   ")
-        left_list.append(int(left))
-        right_list.append(int(right))
+    left_list, right_list = _parse_input_to_left_right_lists(problem_input)
     return sum(
         abs(left - right) for left, right in zip(sorted(left_list), sorted(right_list))
     )
 
 
+def _parse_input_to_left_right_lists(problem_input) -> tuple[list[int], list[int]]:
+    left_list, right_list = [], []
+    for line in problem_input.splitlines():
+        left, right = line.split("   ")
+        left_list.append(int(left))
+        right_list.append(int(right))
+    return left_list, right_list
+
+
 def main_part_two(problem_input: str) -> Any:
-    return
+    left_list, right_list = _parse_input_to_left_right_lists(problem_input)
+    right_counter = Counter(right_list)
+    result = 0
+    for number in left_list:
+        try:
+            occurrence = right_counter[number]
+        except KeyError:
+            occurrence = 0
+        result += number * occurrence
+    return result
 
 
 # def main(problem_input: str) -> Any:
