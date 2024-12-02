b9fd0497a3d98842b8607d5eb878a65aea629ff5
David Jeanjean <david.jeanjean@teampicnic.com>
2024-12-02 15:02:38 +0100

    day 2

---
+++ advent_of_code/y2024/day2/david/solution.py
@@ -0,0 +1,16 @@
+"""Solution for day 2 of Advent of Code 2024, by david."""
+
+from typing import Any
+
+
+def main_part_one(problem_input: str) -> Any:
+
+    for line in problem_input.splitlines():
+        print(line)
+    return
+
+def main_part_two(problem_input: str) -> Any:
+    return
+
+# def main(problem_input: str) -> Any:
+#    return
