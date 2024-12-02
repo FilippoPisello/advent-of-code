57d4018d6599ef3299a330246e1767daf01466f1
Filippo Pisello <66630817+FilippoPisello@users.noreply.github.com>
2024-12-02 12:39:04 +0100

    Fix David's solution (#19)

--- advent_of_code/y2024/day1/david/solution.py
+++ advent_of_code/y2024/day1/david/solution.py
@@ -1,33 +1,34 @@
 import time
 
-t0 = time.time()
-with open("input.txt", "r") as file:
-    data = file.readlines()
 
-right_side = []
-left_side = []
+def main(problem_input: str) -> None:
+    t0 = time.time()
+    data = problem_input.splitlines()
+    right_side = []
+    left_side = []
 
-for i in data:
-    left, right = i.split()
-    left_side.append(int(left))
-    right_side.append(int(right))
+    for i in data:
+        left, right = i.split()
+        left_side.append(int(left))
+        right_side.append(int(right))
 
+    left_side = sorted(left_side)
+    right_side = sorted(right_side)
+    distance = 0
 
-left_side = sorted(left_side)
-right_side = sorted(right_side)
-distance = 0
+    for i in range(len(left_side)):
+        distance += abs(left_side[i - 1] - right_side[i - 1])
 
-for i in range(len(left_side)):
-    distance += abs(left_side[i-1] - right_side[i-1])
+    similarity = 0
 
-similarity = 0
+    for position_left_side_tmtc_la_famille in left_side:
+        similarity += position_left_side_tmtc_la_famille * right_side.count(
+            position_left_side_tmtc_la_famille
+        )
 
-for position_left_side_tmtc_la_famille in left_side:
-    similarity += position_left_side_tmtc_la_famille * right_side.count(position_left_side_tmtc_la_famille)
+    t1 = time.time()
 
-t1= time.time()
+    print(distance)  ## answer part 1
+    print(similarity)  ## answer part 2
 
-print(distance) ## answer part 1
-print(similarity) ## answer part 2
-
-print("script run time: "+ str(round(t1-t0,5)) + " seconds")
+    print("script run time: " + str(round(t1 - t0, 5)) + " seconds")
