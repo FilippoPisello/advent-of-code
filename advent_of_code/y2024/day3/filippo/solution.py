b4f035df3e9ca0a50702640e723d068d0f97e128
FilippoPisello <filippo.pisello@live.it>
2024-12-03 18:55:43 +0100

    Failing part 2

--- advent_of_code/y2024/day3/filippo/solution.py
+++ advent_of_code/y2024/day3/filippo/solution.py
@@ -3,10 +3,12 @@
 import re
 from typing import Any
 
+MUL_PATTERN = r"mul\((\d+),(\d+)\)"
+
 
 def main_part_one(problem_input: str) -> Any:
-    pattern = re.compile(r"mul\((\d+),(\d+)\)")
-    matches = pattern.findall(problem_input)
+    reg = re.compile(MUL_PATTERN)
+    matches = reg.findall(problem_input)
     result = 0
     for match in matches:
         result += int(match[0]) * int(match[1])
@@ -14,7 +16,25 @@ def main_part_one(problem_input: str) -> Any:
 
 
 def main_part_two(problem_input: str) -> Any:
-    return
+    reg = re.compile(".*" + MUL_PATTERN)
+    do_pattern = "do()"
+    dont_pattern = "don't()"
+
+    do = True
+    result = 0
+    start_index = 0
+    for current_index, _ in enumerate(problem_input, start=1):
+        substr = problem_input[start_index:current_index]
+        if do_pattern in substr:
+            do = True
+            start_index = current_index
+        elif dont_pattern in substr:
+            do = False
+            start_index = current_index
+        elif (mul := reg.match(substr)) and do:
+            result += int(mul[1]) * int(mul[2])
+            start_index = current_index
+    return result
 
 
 # def main(problem_input: str) -> Any:
