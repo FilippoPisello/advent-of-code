30203fccd8f369b5f8e2676338c645693bf0cd83
FilippoPisello <filippo.pisello@live.it>
2024-12-03 19:11:59 +0100

    Filippo, day 2, part 2

--- advent_of_code/y2024/day3/filippo/solution.py
+++ advent_of_code/y2024/day3/filippo/solution.py
@@ -7,33 +7,28 @@ MUL_PATTERN = r"mul\((\d{1,3}),(\d{1,3})\)"
 
 
 def main_part_one(problem_input: str) -> Any:
-    reg = re.compile(MUL_PATTERN)
-    matches = reg.findall(problem_input)
+    matches = re.finditer(MUL_PATTERN, problem_input)
     result = 0
     for match in matches:
-        result += int(match[0]) * int(match[1])
+        result += int(match[1]) * int(match[2])
     return result
 
 
 def main_part_two(problem_input: str) -> Any:
-    reg = re.compile(".*" + MUL_PATTERN)
+    mul_matches = re.finditer(MUL_PATTERN, problem_input)
+
     do_pattern = "do()"
     dont_pattern = "don't()"
+    do_indexes = [-1] + [m.start() for m in re.finditer(do_pattern, problem_input)]
+    dont_indexes = [-2] + [m.start() for m in re.finditer(dont_pattern, problem_input)]
 
-    do = True
     result = 0
-    start_index = 0
-    for current_index, _ in enumerate(problem_input, start=1):
-        substr = problem_input[start_index:current_index]
-        if do_pattern in substr:
-            do = True
-            start_index = current_index
-        elif dont_pattern in substr:
-            do = False
-            start_index = current_index
-        elif (mul := reg.match(substr)) and do:
-            result += int(mul[1]) * int(mul[2])
-            start_index = current_index
+    for match in mul_matches:
+        closest_do = max(i for i in do_indexes if i < match.start())
+        closest_dont = max(i for i in dont_indexes if i < match.start())
+        if closest_do > closest_dont:
+            result += int(match[1]) * int(match[2])
+
     return result
 
 
