4403e80be2f4daf55769270c0e7b18203133c536
Filippo Pisello <66630817+FilippoPisello@users.noreply.github.com>
2024-12-03 12:44:03 +0100

    Make solution template a file (#27)

--- advent_of_code/__init__.py
+++ advent_of_code/__init__.py
@@ -2,4 +2,5 @@ from pathlib import Path
 
 MODULE_DIR = Path(__file__).parent.resolve()
 MODULE_NAME = "advent_of_code"
+SOLUTION_TEMPLATE_PATH = MODULE_DIR / "solution_template.py"
 CURRENT_EDITION = 2024
