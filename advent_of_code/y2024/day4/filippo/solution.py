201a7cec37a0727ba87e0126484621af22807134
FilippoPisello <filippo.pisello@live.it>
2024-12-05 00:32:05 +0100

    Minor improvements

--- advent_of_code/y2024/day4/filippo/solution.py
+++ advent_of_code/y2024/day4/filippo/solution.py
@@ -24,18 +24,13 @@ def _get_horizontal_slices(text: str) -> list[str]:
 
 def _get_vertical_slices(text: str) -> list[str]:
     rows = text.splitlines()
-    slices = []
-    for x, _ in enumerate(rows[0]):
-        _slice = ""
-        for row in rows:
-            _slice += row[x]
-        slices.append(_slice)
-    return slices
+    return ["".join([row[x] for row in rows]) for x, _ in enumerate(rows[0])]
 
 
 def _get_diagonal_slices(text: str) -> list[str]:
     rows = text.splitlines()
     slices = []
+    # Start from the bottom left corner
     y = len(rows)
     x = 0
     while x <= len(rows[0]):
@@ -50,6 +45,7 @@ def _get_diagonal_slices(text: str) -> list[str]:
             except IndexError:
                 slices.append(_slice)
                 slices.append(_inverse_slice)
+                # When you get in 0, 0 you need to start moving right
                 if y > 0:
                     y -= 1
                 else:
