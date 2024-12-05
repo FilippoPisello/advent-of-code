5e71ab289c4eaff3a6ec2d9894fb4468bf0d79c9
Filippo Pisello <66630817+FilippoPisello@users.noreply.github.com>
2024-12-05 22:28:24 +0100

    Solution day 5, Filippo (#33)
    * Part 1, test pass
    
    * Brute force it
    
    * Better brute forcing
    
    * Fix type hints

---
+++ advent_of_code/y2024/day5/filippo/tests.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import pytest
+
+from advent_of_code.y2024.day5.filippo.solution import main_part_one, main_part_two
+
+SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            143,
+        ),
+    ),
+)
+def test_cases_part_one(problem_input, expected):
+    assert main_part_one(problem_input) == expected
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            123,
+        ),
+    ),
+)
+def test_cases_part_two(problem_input, expected):
+    assert main_part_two(problem_input) == expected
