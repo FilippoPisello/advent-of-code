495bf7f5a778001dddaadd07be878470c837e328
MaximeBizet <maxime.bizet@teampicnic.com>
2024-12-05 17:50:51 +0100

    Run black

--- advent_of_code/y2024/day3/david/solution.py
+++ advent_of_code/y2024/day3/david/solution.py
@@ -4,26 +4,29 @@ from typing import Any
 import re
 import numpy as np
 
+
 def main_part_one(problem_input: str) -> Any:
-    program = np.array(re.findall(r"mul\(\d{1,3},\d{1,3}\)",problem_input))
+    program = np.array(re.findall(r"mul\(\d{1,3},\d{1,3}\)", problem_input))
     total = 0
     for mul in program:
-        x = re.findall(r"\d{1,3}",mul)
-        total += int(x[0])*int(x[1])
-    
+        x = re.findall(r"\d{1,3}", mul)
+        total += int(x[0]) * int(x[1])
+
     return total
 
+
 def main_part_two(problem_input: str) -> Any:
-    program = np.array(re.findall(r"mul\(\d{1,3},\d{1,3}\)|don't|do",problem_input))
+    program = np.array(re.findall(r"mul\(\d{1,3},\d{1,3}\)|don't|do", problem_input))
     do_or_dont_that_is_the_question = "do"
     total = 0
     for mul in program:
-        if mul in ("do","don't"):
+        if mul in ("do", "don't"):
             do_or_dont_that_is_the_question = mul
-        if do_or_dont_that_is_the_question == "do" and len(mul)>5:
-            x = re.findall(r"\d{1,3}",mul)
-            total += int(x[0])*int(x[1])
+        if do_or_dont_that_is_the_question == "do" and len(mul) > 5:
+            x = re.findall(r"\d{1,3}", mul)
+            total += int(x[0]) * int(x[1])
     return total
+
 
 # def main(problem_input: str) -> Any:
 #    return
--- advent_of_code/y2024/day4/david/solution.py
+++ advent_of_code/y2024/day4/david/solution.py
@@ -5,52 +5,67 @@ from typing import Any
 
 def main_part_one(problem_input: str) -> Any:
     word_search = problem_input.split("\n")
-    total  = 0
-    for row,word in enumerate(word_search):
+    total = 0
+    for row, word in enumerate(word_search):
         for column in range(len(word)):
-            if word[column] == 'X':
-                total += all_direction_word(column,row,word_search).count("XMAS")
+            if word[column] == "X":
+                total += all_direction_word(column, row, word_search).count("XMAS")
     return total
 
-def all_direction_word(column,row,puzzle):
+
+def all_direction_word(column, row, puzzle):
     directions = {
-        "N": (-1, 0), "NE": (-1, 1), "E": (0, 1), "SE": (1, 1),
-        "S": (1, 0), "SW": (1, -1), "W": (0, -1), "NW": (-1, -1)
+        "N": (-1, 0),
+        "NE": (-1, 1),
+        "E": (0, 1),
+        "SE": (1, 1),
+        "S": (1, 0),
+        "SW": (1, -1),
+        "W": (0, -1),
+        "NW": (-1, -1),
     }
 
     rows = len(puzzle)
     columns = len(puzzle[0])
     words = []
 
-    for direction,(row_dir,col_dir) in directions.items():
+    for direction, (row_dir, col_dir) in directions.items():
         word = "X"
-        r,c = row, column
+        r, c = row, column
         for i in range(3):
             r += row_dir
             c += col_dir
-            if (0 <= r < rows and 0 <= c < columns):
+            if 0 <= r < rows and 0 <= c < columns:
                 word += puzzle[r][c]
             else:
                 break
         words.append(word)
     return words
 
+
 def main_part_two(problem_input: str) -> Any:
     word_search = problem_input.split("\n")
     total = 0
     rows = len(word_search)
     columns = len(word_search[0])
-    for r,word in enumerate(word_search):
+    for r, word in enumerate(word_search):
         for c in range(len(word)):
-            if word[c] == 'A':
-                if (0 < r < rows-1 and 0 < c < columns-1):
-                    word_1 = word_search[r-1][c-1]+word_search[r][c]+word_search[r+1][c+1]
-                    word_2 = word_search[r-1][c+1]+word_search[r][c]+word_search[r+1][c-1]
-                    if word_1 in ("MAS","SAM") and word_2 in ("MAS","SAM"):
-                        total +=1
-                
-    return total
+            if word[c] == "A":
+                if 0 < r < rows - 1 and 0 < c < columns - 1:
+                    word_1 = (
+                        word_search[r - 1][c - 1]
+                        + word_search[r][c]
+                        + word_search[r + 1][c + 1]
+                    )
+                    word_2 = (
+                        word_search[r - 1][c + 1]
+                        + word_search[r][c]
+                        + word_search[r + 1][c - 1]
+                    )
+                    if word_1 in ("MAS", "SAM") and word_2 in ("MAS", "SAM"):
+                        total += 1
 
+    return total
 
 
 # def main(problem_input: str) -> Any:
