eed0f19e0eea4c171f906a2e0da91f048b7a435b
FilippoPisello <filippo.pisello@live.it>
2024-12-07 13:31:45 +0100

    Prepopulate test file with setup for provided test case

--- advent_of_code/__init__.py
+++ advent_of_code/__init__.py
@@ -2,5 +2,6 @@ from pathlib import Path
 
 MODULE_DIR = Path(__file__).parent.resolve()
 MODULE_NAME = "advent_of_code"
-SOLUTION_TEMPLATE_PATH = MODULE_DIR / "solution_template.py"
+TEMPLATE_SOLUTION_PATH = MODULE_DIR / "template_solution.py"
+TEMPLATE_TESTS_PATH = MODULE_DIR / "template_tests.py"
 CURRENT_EDITION = 2024
