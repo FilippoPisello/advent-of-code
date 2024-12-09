81d7cbe621100e516a2916208032e7c0ad1c3e4d
FilippoPisello <filippo.pisello@live.it>
2024-12-09 19:52:24 +0100

    Setup

---
+++ advent_of_code/y2024/day9/filippo/solution.py
@@ -0,0 +1,15 @@
+"""Solution for day 9 of Advent of Code 2024, by filippo."""
+
+from typing import Any
+
+
+def main_part_one(problem_input: str) -> Any:
+    return
+
+
+def main_part_two(problem_input: str) -> Any:
+    return
+
+
+# def main(problem_input: str) -> Any:
+#    return
