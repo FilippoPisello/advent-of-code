6eb3c07a6c7a45c4713f93e31d0cb33c03915691
FilippoPisello <filippo.pisello@live.it>
2024-12-09 20:45:43 +0100

    Pass test case

--- advent_of_code/y2024/day9/filippo/solution.py
+++ advent_of_code/y2024/day9/filippo/solution.py
@@ -1,10 +1,58 @@
 """Solution for day 9 of Advent of Code 2024, by filippo."""
 
+import re
+from pathlib import Path
 from typing import Any
+
+STR_FILE_PATH = Path(__file__).parent / "solution.txt"
 
 
 def main_part_one(problem_input: str) -> Any:
-    return
+    decoded_str = _decode(problem_input)
+    while _has_fillable_memory(decoded_str):
+        decoded_str = _shift_one_memory_unit(decoded_str)
+    return _compute_checksum(decoded_str)
+
+
+def _decode(encoded_str: str) -> str:
+    decoded_str = ""
+    odd_chars, even_chars = encoded_str[::2], encoded_str[1::2]
+    for index, (odd_char, even_char) in enumerate(zip(odd_chars, even_chars)):
+        decoded_str += str(index) * int(odd_char)
+        decoded_str += "." * int(even_char)
+    if len(encoded_str) % 2:
+        decoded_str += str(len(even_chars)) * int(encoded_str[-1])
+    return decoded_str
+
+
+def _has_fillable_memory(decoded_str: str) -> bool:
+    leftmost_dot_index = decoded_str.find(".")
+    # If there are only dots after the leftmost dot, the memory is not fillable
+    if set(decoded_str[leftmost_dot_index + 1 :]) == {"."}:
+        return False
+    return True
+
+
+def _shift_one_memory_unit(decoded_str: str) -> str:
+    leftmost_dot_index = decoded_str.find(".")
+    rightmost_digit_index = re.search(r"\d(?=\D*$)", decoded_str).start()
+    assert leftmost_dot_index < rightmost_digit_index
+    return (
+        decoded_str[:leftmost_dot_index]
+        + decoded_str[rightmost_digit_index]
+        + decoded_str[leftmost_dot_index + 1 : rightmost_digit_index]
+        + "."
+        + decoded_str[rightmost_digit_index + 1 :]
+    )
+
+
+def _compute_checksum(decoded_str: str) -> int:
+    checksum = 0
+    for index, char in enumerate(decoded_str):
+        if char == ".":
+            break
+        checksum += index * int(char)
+    return checksum
 
 
 def main_part_two(problem_input: str) -> Any:
--- advent_of_code/y2024/day9/filippo/tests.py
+++ advent_of_code/y2024/day9/filippo/tests.py
@@ -4,9 +4,68 @@ from pathlib import Path
 
 import pytest
 
-from advent_of_code.y2024.day9.filippo.solution import main_part_one, main_part_two
+from advent_of_code.y2024.day9.filippo.solution import (
+    _compute_checksum,
+    _decode,
+    _has_fillable_memory,
+    _shift_one_memory_unit,
+    main_part_one,
+    main_part_two,
+)
 
 SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            "12345",
+            "0..111....22222",
+        ),
+        (
+            "123451",
+            "0..111....22222.",
+        ),
+        (
+            "2333133121414131402",
+            "00...111...2...333.44.5555.6666.777.888899",
+        ),
+    ),
+)
+def test_decode(problem_input, expected):
+    assert _decode(problem_input) == expected
+
+
+def test_has_fillable_memory():
+    assert _has_fillable_memory("0..111....22222") == True
+    assert _has_fillable_memory("0..111....22222.") == True
+    assert _has_fillable_memory("022111222......") == False
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            "0..111....22222",
+            "02.111....2222.",
+        ),
+        (
+            "02211122..2....",
+            "022111222......",
+        ),
+        (
+            "0099811188.2...333.44.5555.6666.777.8.....",
+            "009981118882...333.44.5555.6666.777.......",
+        ),
+    ),
+)
+def test_shift_one_memory_unit(problem_input, expected):
+    assert _shift_one_memory_unit(problem_input) == expected
+
+
+def test_compute_checksum():
+    assert _compute_checksum("0099811188827773336446555566..............") == 1928
 
 
 @pytest.mark.parametrize(
