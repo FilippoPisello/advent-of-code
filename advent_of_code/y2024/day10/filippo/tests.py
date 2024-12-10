7ebb04403e87a397a08dcac0daf6433ee15534b8
Filippo Pisello <filippo.pisello@live.it>
2024-12-10 20:34:59 +0100

    Setup

---
+++ advent_of_code/y2024/day10/filippo/tests.py
@@ -0,0 +1,35 @@
+"""Tests for day 10 of Advent of Code 2024, by filippo."""
+
+from pathlib import Path
+
+import pytest
+
+from advent_of_code.y2024.day10.filippo.solution import main_part_one, main_part_two
+
+SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            36,
+        ),
+    ),
+)
+def test_cases_part_one(problem_input, expected):
+    assert main_part_one(problem_input) == expected
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            "expected_result_part_one",
+        ),
+    ),
+)
+def test_cases_part_two(problem_input, expected):
+    assert main_part_two(problem_input) == expected
