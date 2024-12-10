e8ead4d82ec8887be84ade28cb44db8a72a8ba83
FilippoPisello <filippo.pisello@live.it>
2024-12-10 09:26:16 +0100

    Performance tweak

--- advent_of_code/y2024/day9/filippo/solution.py
+++ advent_of_code/y2024/day9/filippo/solution.py
@@ -12,23 +12,6 @@ def main_part_one(problem_input: str) -> Any:
     return _compute_checksum(decoded_str)
 
 
-def _compact(decoded_str: list[str]) -> list[str]:
-    rightend_index = len(decoded_str) - 1
-    for index, char in enumerate(decoded_str):
-        if set(decoded_str[index:]) == {"."}:
-            break
-        if char != ".":
-            continue
-        while decoded_str[rightend_index] == ".":
-            rightend_index -= 1
-        # Swap the two characters
-        decoded_str[index], decoded_str[rightend_index] = (
-            decoded_str[rightend_index],
-            decoded_str[index],
-        )
-    return decoded_str
-
-
 def _decode(encoded_str: str) -> list[str]:
     decoded_str = []
     odd_chars, even_chars = encoded_str[::2], encoded_str[1::2]
@@ -37,6 +20,23 @@ def _decode(encoded_str: str) -> list[str]:
         decoded_str += "." * int(even_char)
     if len(encoded_str) % 2:
         decoded_str += str(len(even_chars)) * int(encoded_str[-1])
+    return decoded_str
+
+
+def _compact(decoded_str: list[str]) -> list[str]:
+    rightend_index = len(decoded_str) - 1
+    for index, char in enumerate(decoded_str):
+        if set(decoded_str[index : rightend_index + 1]) == {"."}:
+            break
+        if char != ".":
+            continue
+        while (decoded_str[rightend_index] == ".") and (rightend_index > index):
+            rightend_index -= 1
+        # Swap the two characters
+        decoded_str[index], decoded_str[rightend_index] = (
+            decoded_str[rightend_index],
+            decoded_str[index],
+        )
     return decoded_str
 
 
