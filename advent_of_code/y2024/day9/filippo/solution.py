4aacc37d3923b5b20abfd4558d257dddb1fc3eb5
Filippo Pisello <filippo.pisello@live.it>
2024-12-10 22:55:29 +0100

    Solution part 1

--- advent_of_code/y2024/day9/filippo/solution.py
+++ advent_of_code/y2024/day9/filippo/solution.py
@@ -7,19 +7,20 @@ STR_FILE_PATH = Path(__file__).parent / "solution.txt"
 
 
 def main_part_one(problem_input: str) -> Any:
+    problem_input: list[int] = list(map(int, problem_input))
     decoded_str = _decode(problem_input)
     decoded_str = _compact(decoded_str)
     return _compute_checksum(decoded_str)
 
 
-def _decode(encoded_str: str) -> list[str]:
+def _decode(encoded_str: list[int]) -> list[str]:
     decoded_str = []
     odd_chars, even_chars = encoded_str[::2], encoded_str[1::2]
     for index, (odd_char, even_char) in enumerate(zip(odd_chars, even_chars)):
-        decoded_str += str(index) * int(odd_char)
-        decoded_str += "." * int(even_char)
+        decoded_str.extend([str(index)] * odd_char)
+        decoded_str += "." * even_char
     if len(encoded_str) % 2:
-        decoded_str += str(len(even_chars)) * int(encoded_str[-1])
+        decoded_str.extend([str(len(even_chars))] * encoded_str[-1])
     return decoded_str
 
 
@@ -32,11 +33,13 @@ def _compact(decoded_str: list[str]) -> list[str]:
             continue
         while (decoded_str[rightend_index] == ".") and (rightend_index > index):
             rightend_index -= 1
+            print(rightend_index, end="\r")
         # Swap the two characters
         decoded_str[index], decoded_str[rightend_index] = (
             decoded_str[rightend_index],
             decoded_str[index],
         )
+
     return decoded_str
 
 
