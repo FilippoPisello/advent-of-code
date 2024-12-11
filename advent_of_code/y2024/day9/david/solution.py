32c439592c26dfc7c65689e5a814a859ea380407
David Jeanjean <david.jeanjean@teampicnic.com>
2024-12-11 22:08:07 +0100

    vew

--- advent_of_code/y2024/day9/david/solution.py
+++ advent_of_code/y2024/day9/david/solution.py
@@ -45,8 +45,26 @@ def main_part_two(problem_input: str) -> Any:
 
     string_list = [list(x) for x in problem_input.split()]
     disk_map = string_list[0]
+    disk_map = first_step(disk_map)
+    chars_nbr = len(disk_map)
+    for index, file_block in enumerate(disk_map):
+        print(index)
+        if all(x == -1 for x in disk_map[index:]):
+            break
+        if file_block == -1:
+            reverse_map = disk_map[::-1]
+            for reverse_index, file_reverse_block in enumerate(reverse_map):
+                if file_reverse_block != -1:
+                    disk_map[index] = file_reverse_block
+                    disk_map[chars_nbr - 1 - reverse_index] = -1
+                    break
 
-    return
+    total = 0
+    for x, y in enumerate(disk_map):
+        if y > 0:
+            total += x * y
+
+    return total
 
 
 # def main(problem_input: str) -> Any:
