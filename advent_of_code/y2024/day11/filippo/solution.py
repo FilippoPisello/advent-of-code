0e04978bb0d8cbaad96a745a9b6073aaf0214928
FilippoPisello <filippo.pisello@live.it>
2024-12-14 17:26:26 +0100

    Solution part 2

--- advent_of_code/y2024/day11/filippo/solution.py
+++ advent_of_code/y2024/day11/filippo/solution.py
@@ -1,17 +1,32 @@
 """Solution for day 11 of Advent of Code 2024, by filippo."""
 
+from functools import partial
 from typing import Any
 
 
-def main_part_one(problem_input: str) -> Any:
+def main(problem_input: str, iterations: int) -> Any:
     stones = [int(x) for x in problem_input.split(" ")]
-    for _ in range(25):
-        new_stones = []
-        for stone in stones:
-            for res in _blink(stone):
-                new_stones.append(res)
-        stones = new_stones
-    return len(stones)
+    counter = {}
+    _add_stones_to_counter(stones, counter)
+    for n in range(iterations):
+        print(n, end="\r", flush=True)
+        new_counter = {}
+        for stone, count in counter.items():
+            new_stones = _blink(stone)
+            for stone in new_stones:
+                if stone not in new_counter:
+                    new_counter[stone] = 0
+                new_counter[stone] += count
+        counter = new_counter
+    return sum(counter.values())
+
+
+def _add_stones_to_counter(stones: list[int], counter: dict[int, int]) -> None:
+    for stone in stones:
+        if stone not in counter:
+            counter[stone] = 1
+        else:
+            counter[stone] += 1
 
 
 def _blink(n: int) -> list[int]:
@@ -24,9 +39,5 @@ def _blink(n: int) -> list[int]:
     return [2024 * n]
 
 
-def main_part_two(problem_input: str) -> Any:
-    return
-
-
-# def main(problem_input: str) -> Any:
-#    return
+main_part_one = partial(main, iterations=25)
+main_part_two = partial(main, iterations=75)
--- advent_of_code/y2024/day11/filippo/tests.py
+++ advent_of_code/y2024/day11/filippo/tests.py
@@ -4,9 +4,27 @@ from pathlib import Path
 
 import pytest
 
-from advent_of_code.y2024.day11.filippo.solution import main_part_one, main_part_two
+from advent_of_code.y2024.day11.filippo.solution import (
+    main,
+    main_part_one,
+    main_part_two,
+)
 
 SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected", "iterations"),
+    (
+        (
+            "125 17",
+            3,
+            1,
+        ),
+    ),
+)
+def test_cases_manual(problem_input, expected, iterations):
+    assert main(problem_input, iterations) == expected
 
 
 @pytest.mark.parametrize(
@@ -20,17 +38,3 @@ SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
 )
 def test_cases_part_one(problem_input, expected):
     assert main_part_one(problem_input) == expected
-
-
-@pytest.mark.skip
-@pytest.mark.parametrize(
-    ("problem_input", "expected"),
-    (
-        (
-            SAMPLE_INPUT_PATH.read_text(),
-            "expected_result_part_one",
-        ),
-    ),
-)
-def test_cases_part_two(problem_input, expected):
-    assert main_part_two(problem_input) == expected
