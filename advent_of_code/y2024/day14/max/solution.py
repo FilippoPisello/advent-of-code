c50cca16353d6e6d48ec589b26bcaa5c0c8fc651
MaximeBizet <picnic@11X0BK3>
2024-12-14 17:01:35 +0100

    Solution day 14, Max: set-up project skeleton

---
+++ advent_of_code/y2024/day14/max/solution.py
@@ -0,0 +1,15 @@
+"""Solution for day 14 of Advent of Code 2024, by max."""
+
+from typing import Any
+
+
+def main_part_one(problem_input: str) -> Any:
+    return
+
+
+def main_part_two(problem_input: str) -> Any:
+    return
+
+
+# def main(problem_input: str) -> Any:
+#    return
