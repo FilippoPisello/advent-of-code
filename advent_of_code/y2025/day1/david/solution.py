168664cca9f5d490802bcfe2fc49b56cd8f1d662
David Jeanjean <90677875+David99jj@users.noreply.github.com>
2025-12-01 18:41:03 +0100

    2025 day 1 David solution (#55)

---
+++ advent_of_code/y2025/day1/david/solution.py
@@ -0,0 +1,41 @@
+import math
+
+
+def main_part_one(problem_input: str) -> None:
+
+    dial_a = 50
+    solution_1 = 0
+
+    for line in problem_input.splitlines():
+
+        dial = int(line.replace("L", "-").replace("R", ""))
+        dial_b = dial_a + dial
+
+        if dial_b % 100 == 0:
+            solution_1 += 1
+
+        dial_a = dial_b
+
+    print(solution_1)
+
+
+def main_part_two(problem_input: str) -> None:
+
+    dial_a = 50
+    solution_2 = 0
+
+    for line in problem_input.splitlines():
+
+        dial = int(line.replace("L", "-").replace("R", ""))
+        dial_b = dial_a + dial
+
+        if dial_a % 100 == 0:
+            solution_2 += abs(dial) // 100
+        elif dial_b % 100 == 0:
+            solution_2 += 1 + abs(dial) // 100
+        else:
+            solution_2 += abs(dial_a // 100 - dial_b // 100)
+
+        dial_a = dial_b
+
+    print(solution_2)
