0376e144abe40ad9603d131eb5e6c0c5d56d2476
FilippoPisello <filippo.pisello@live.it>
2025-12-01 10:39:50 +0100

    Update current year

--- advent_of_code/__init__.py
+++ advent_of_code/__init__.py
@@ -4,4 +4,4 @@ MODULE_DIR = Path(__file__).parent.resolve()
 MODULE_NAME = "advent_of_code"
 TEMPLATE_SOLUTION_PATH = MODULE_DIR / "template_solution.py"
 TEMPLATE_TESTS_PATH = MODULE_DIR / "template_tests.py"
-CURRENT_EDITION = 2024
+CURRENT_EDITION = 2025
