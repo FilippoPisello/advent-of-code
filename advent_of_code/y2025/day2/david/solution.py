d90c4820daf6d9804849a9c31a5196f27fe69771
David99jj <david.jeanjean@teampicnic.com>
2025-12-02 14:21:22 +0100

    part 2 solution

--- advent_of_code/y2025/day2/david/solution.py
+++ advent_of_code/y2025/day2/david/solution.py
@@ -24,6 +24,27 @@ def main_part_one(problem_input: str) -> Any:
 
 
 def main_part_two(problem_input: str) -> Any:
+
+    input_formatted = [
+        list(map(int, part.split("-"))) for part in problem_input.split(",")
+    ]
+    counter = 0
+
+    for ids in input_formatted:
+        for id in range(ids[0], ids[1] + 1):
+            id_length = len(str(id))
+            for split_size in range(1, 1 + id_length // 2):
+                if id_length % split_size == 0:
+                    split_list = [
+                        str(id)[i : i + split_size]
+                        for i in range(0, id_length, split_size)
+                    ]
+                    if len(set(split_list)) == 1:
+                        counter += id
+                        print(id)
+                        break
+
+    print(counter)
     return
 
 
