8ef2c2759c00953eb7c2bb22b8fea1077df8954d
FilippoPisello <filippo.pisello@live.it>
2025-12-02 13:43:23 +0100

    Refactor

--- advent_of_code/y2025/day2/filippo/solution.py
+++ advent_of_code/y2025/day2/filippo/solution.py
@@ -9,16 +9,20 @@ def main_part_one(problem_input: str) -> Any:
     for id_range in ranges:
         range_start, range_end = map(int, id_range.split("-"))
         for id_ in range(range_start, range_end + 1):
-            id_str = str(id_)
-            if len(id_str) % 2 != 0:
-                continue
-            first_half, second_half = (
-                id_str[: len(id_str) // 2],
-                id_str[len(id_str) // 2 :],
-            )
-            if first_half == second_half:
+            if _is_invalid_half_only(id_):
                 sum_invalid += id_
     return sum_invalid
+
+
+def _is_invalid_half_only(id_: int) -> bool:
+    id_str = str(id_)
+    if len(id_str) % 2 != 0:
+        return False
+    first_half, second_half = (
+        id_str[: len(id_str) // 2],
+        id_str[len(id_str) // 2 :],
+    )
+    return first_half == second_half
 
 
 def main_part_two(problem_input: str) -> Any:
