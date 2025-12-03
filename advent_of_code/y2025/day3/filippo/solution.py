d8ff5318de924e7dd517153e7c26169eb23627ae
FilippoPisello <filippo.pisello@live.it>
2025-12-03 19:18:03 +0100

    Solution part 1 (0.00373s)

--- advent_of_code/y2025/day3/filippo/solution.py
+++ advent_of_code/y2025/day3/filippo/solution.py
@@ -4,7 +4,23 @@ from typing import Any
 
 
 def main_part_one(problem_input: str) -> Any:
-    return
+    joltage = 0
+
+    banks = problem_input.splitlines()
+    for bank in banks:
+        bank_record = 0
+        batteries = [int(battery) for battery in list(bank)]
+        highest_possible_ten = max(batteries[:-1])
+        for index, battery in enumerate(batteries[:-1]):
+            if battery == highest_possible_ten:
+                higest_possible_unit = max(batteries[index + 1 :])
+                possible_combination = int(
+                    f"{highest_possible_ten}{higest_possible_unit}"
+                )
+                if possible_combination > bank_record:
+                    bank_record = possible_combination
+        joltage += bank_record
+    return joltage
 
 
 def main_part_two(problem_input: str) -> Any:
--- advent_of_code/y2025/day3/filippo/tests.py
+++ advent_of_code/y2025/day3/filippo/tests.py
@@ -12,16 +12,15 @@ SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
 @pytest.mark.parametrize(
     ("problem_input", "expected"),
     (
-        (
-            SAMPLE_INPUT_PATH.read_text(),
-            "expected_result_part_one",
-        ),
+        ("987654321111111", 98),
+        (SAMPLE_INPUT_PATH.read_text(), 357),
     ),
 )
 def test_cases_part_one(problem_input, expected):
     assert main_part_one(problem_input) == expected
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize(
     ("problem_input", "expected"),
     (
