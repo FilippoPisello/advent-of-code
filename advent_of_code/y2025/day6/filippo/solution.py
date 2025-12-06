28a776b89013ede5e33341d9207ab37494e82ac3
Filippo Pisello <filippo.pisello@live.it>
2025-12-06 10:47:15 +0100

    Solution part 1 (0.00605s)

--- advent_of_code/y2025/day6/filippo/solution.py
+++ advent_of_code/y2025/day6/filippo/solution.py
@@ -4,7 +4,23 @@ from typing import Any
 
 
 def main_part_one(problem_input: str) -> Any:
-    return
+    total = 0
+    values: dict[int, list[int]] = {}
+    for row in problem_input.splitlines():
+        for index, value in enumerate(row.split()):
+            if value.isdigit():
+                if index not in values:
+                    values[index] = []
+                values[index].append(int(value))
+            else:
+                if value == "+":
+                    total += sum(values[index])
+                elif value == "*":
+                    prod = 1
+                    for v in values[index]:
+                        prod *= v
+                    total += prod
+    return total
 
 
 def main_part_two(problem_input: str) -> Any:
