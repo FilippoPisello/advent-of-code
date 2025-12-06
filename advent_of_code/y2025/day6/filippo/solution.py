e4f07b6811d555f47accec62270471177eb93109
Filippo Pisello <66630817+FilippoPisello@users.noreply.github.com>
2025-12-06 18:59:40 +0100

    Solution day 6, Filippo (#74)
    * Setup
    
    * Solution part 1 (0.00605s)
    
    * Almost there need to go biking
    
    * Solution part 2 (0.01954s)

---
+++ advent_of_code/y2025/day6/filippo/solution.py
@@ -0,0 +1,93 @@
+"""Solution for day 6 of Advent of Code 2025, by filippo."""
+
+from typing import Any
+from unicodedata import digit
+
+
+def main_part_one(problem_input: str) -> Any:
+    total = 0
+    values: dict[int, list[int]] = {}
+    for row in problem_input.splitlines():
+        for index, value in enumerate(row.split()):
+            if value.isdigit():
+                if index not in values:
+                    values[index] = []
+                values[index].append(int(value))
+            else:
+                if value == "+":
+                    total += sum(values[index])
+                elif value == "*":
+                    prod = 1
+                    for v in values[index]:
+                        prod *= v
+                    total += prod
+    return total
+
+
+def main_part_two(problem_input: str) -> Any:
+    total = 0
+    values: dict[int, list[int]] = {}
+    problems = []
+
+    lines = problem_input.splitlines()
+    signs = extract_signs(lines[-1])
+
+    for values, sign in zip(split_value_columns(problem_input), signs, strict=True):
+        recoded_values = recode_values(values)
+
+        if sign == "+":
+            total += sum(recoded_values)
+        elif sign == "*":
+            prod = 1
+            for v in recoded_values:
+                prod *= v
+            total += prod
+
+    return total
+
+
+def extract_signs(line: str):
+    return [char.strip() for char in line.split() if char.strip()]
+
+
+def split_value_columns(problem_input: str) -> list[list[str]]:
+    lines = problem_input.splitlines()
+    signs_line = lines[-1]
+    starting_index = 0
+    raw_value_columns: list[list[str]] = []
+    for index, character in enumerate(signs_line[1:], start=1):
+        is_last_character = index == len(signs_line) - 1
+        if (character != " ") or is_last_character:
+            segment_start = starting_index
+            if is_last_character:
+                segment_end = index + 1
+            else:
+                segment_end = index - 1
+            extracted_col = [line[segment_start:segment_end] for line in lines[:-1]]
+            raw_value_columns.append(extracted_col)
+            starting_index = index
+    return raw_value_columns
+
+
+def recode_values(raw_values: list[str]) -> list[int]:
+    destructured_values: list[dict[int, str]] = []
+    max_index = 0
+
+    for value in raw_values:
+        index_map = {}
+
+        for index, digit_ in enumerate(str(value)):
+            index_map[index] = digit_
+            if index > max_index:
+                max_index = index
+        destructured_values.append(index_map)
+
+    recoded_values: list[int] = []
+    for index in range(max_index + 1):
+        recoded_value_str = ""
+        for value_map in destructured_values:
+            if index in value_map:
+                recoded_value_str += value_map[index]
+        recoded_values.append(int(recoded_value_str))
+
+    return recoded_values
---
+++ advent_of_code/y2025/day6/filippo/tests.py
@@ -0,0 +1,81 @@
+"""Tests for day 6 of Advent of Code 2025, by filippo."""
+
+from pathlib import Path
+
+import pytest
+
+from advent_of_code.y2025.day6.filippo.solution import (
+    extract_signs,
+    main_part_one,
+    main_part_two,
+    recode_values,
+    split_value_columns,
+)
+
+SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            4277556,
+        ),
+    ),
+)
+def test_cases_part_one(problem_input, expected):
+    assert main_part_one(problem_input) == expected
+
+
+def test_signs_extraction():
+    signs = "*   +   *   +  "
+    assert extract_signs(signs) == ["*", "+", "*", "+"]
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            [
+                ["123", " 45", "  6"],
+                ["328", "64 ", "98 "],
+                [" 51", "387", "215"],
+                ["64 ", "23 ", "314"],
+            ],
+        ),
+    ),
+)
+def test_columns_splitting(problem_input, expected):
+    assert split_value_columns(problem_input) == expected
+
+
+@pytest.mark.parametrize(
+    ("raw_values", "expected"),
+    (
+        (
+            ["64 ", "23", "314"],
+            [623, 431, 4],
+        ),
+        (
+            [" 51", "387", "215"],
+            [32, 581, 175],
+        ),
+    ),
+)
+def test_values_recoding(raw_values, expected):
+    assert recode_values(raw_values) == expected
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            3263827,
+        ),
+    ),
+)
+def test_cases_part_two(problem_input, expected):
+    assert main_part_two(problem_input) == expected
