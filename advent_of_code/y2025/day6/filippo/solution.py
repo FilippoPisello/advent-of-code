45527e0e8ef5ac5f56bd38b9e3af09c0e810f14b
Filippo Pisello <filippo.pisello@live.it>
2025-12-06 11:34:50 +0100

    Almost there need to go biking

--- advent_of_code/y2025/day6/filippo/solution.py
+++ advent_of_code/y2025/day6/filippo/solution.py
@@ -1,6 +1,7 @@
 """Solution for day 6 of Advent of Code 2025, by filippo."""
 
 from typing import Any
+from unicodedata import digit
 
 
 def main_part_one(problem_input: str) -> Any:
@@ -24,8 +25,53 @@ def main_part_one(problem_input: str) -> Any:
 
 
 def main_part_two(problem_input: str) -> Any:
-    return
+    values: dict[int, list[int]] = {}
+    problems = []
+
+    split_value_columns(problem_input)
+
+    return sum(problems)
 
 
-# def main(problem_input: str) -> Any:
-#    return
+def split_value_columns(problem_input: str) -> list[list[str]]:
+    lines = problem_input.splitlines()
+    signs_line = lines[-1]
+    starting_index = 0
+    raw_value_columns: list[list[str]] = []
+    for index, character in enumerate(signs_line[1:], start=1):
+        is_last_character = index == len(signs_line) - 1
+        if (character != " ") or is_last_character:
+            segment_start = starting_index
+            if is_last_character:
+                segment_end = index + 1
+            else:
+                segment_end = index - 1
+            extracted_col = [line[segment_start:segment_end] for line in lines[:-1]]
+            print(extracted_col)
+            raw_value_columns.append(extracted_col)
+            starting_index = index
+    return raw_value_columns
+
+
+def recode_values(raw_values: list[int]) -> list[int]:
+    destructured_values: list[dict[int, str]] = []
+    max_index = 0
+
+    for value in raw_values:
+        index_map = {}
+
+        for index, digit in enumerate(str(value)):
+            index_map[index] = digit
+            if index > max_index:
+                max_index = index
+        destructured_values.append(index_map)
+
+    recoded_values: list[int] = []
+    for index in range(max_index + 1):
+        recoded_value_str = ""
+        for value_map in destructured_values:
+            if index in value_map:
+                recoded_value_str += value_map[index]
+        recoded_values.append(int(recoded_value_str))
+
+    return recoded_values
--- advent_of_code/y2025/day6/filippo/tests.py
+++ advent_of_code/y2025/day6/filippo/tests.py
@@ -4,7 +4,12 @@ from pathlib import Path
 
 import pytest
 
-from advent_of_code.y2025.day6.filippo.solution import main_part_one, main_part_two
+from advent_of_code.y2025.day6.filippo.solution import (
+    main_part_one,
+    main_part_two,
+    recode_values,
+    split_value_columns,
+)
 
 SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
 
@@ -21,15 +26,52 @@ SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
 def test_cases_part_one(problem_input, expected):
     assert main_part_one(problem_input) == expected
 
-@pytest.mark.skip("Remove this line once part two is implemented")
+
+@pytest.mark.skip(reason="Part two not implemented yet")
 @pytest.mark.parametrize(
     ("problem_input", "expected"),
     (
         (
             SAMPLE_INPUT_PATH.read_text(),
-            "expected_result_part_one",
+            3263827,
         ),
     ),
 )
 def test_cases_part_two(problem_input, expected):
     assert main_part_two(problem_input) == expected
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            [
+                ["123", " 45", "  6"],
+                ["328", "64 ", "98 "],
+                [" 51", "387", "215"],
+                ["64 ", "23 ", "314"],
+            ],
+        ),
+    ),
+)
+def test_columns_splitting(problem_input, expected):
+    assert split_value_columns(problem_input) == expected
+
+
+@pytest.mark.skip(reason="Part two not implemented yet")
+@pytest.mark.parametrize(
+    ("raw_values", "expected"),
+    (
+        (
+            [64, 23, 314],
+            [623, 431, 4],
+        ),
+        (
+            [51, 387, 215],
+            [32, 581, 175],
+        ),
+    ),
+)
+def test_values_recoding(raw_values, expected):
+    assert recode_values(raw_values) == expected
