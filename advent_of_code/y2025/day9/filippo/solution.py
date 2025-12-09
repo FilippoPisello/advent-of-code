7d28853d3ef54dee24638145429dd9df9e5b58c5
FilippoPisello <filippo.pisello@live.it>
2025-12-09 18:49:58 +0100

    Solution part 1 (0.03245s)

--- advent_of_code/y2025/day9/filippo/solution.py
+++ advent_of_code/y2025/day9/filippo/solution.py
@@ -4,7 +4,17 @@ from typing import Any
 
 
 def main_part_one(problem_input: str) -> Any:
-    return
+    coordinates = [
+        tuple(map(int, line.split(","))) for line in problem_input.strip().splitlines()
+    ]
+    max_area = 0
+    for index, (x1, y1) in enumerate(coordinates):
+        for x2, y2 in coordinates[index + 1 :]:
+            area = (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
+            if area > max_area:
+                max_area = area
+
+    return max_area
 
 
 def main_part_two(problem_input: str) -> Any:
