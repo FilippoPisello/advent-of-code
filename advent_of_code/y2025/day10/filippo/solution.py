830a53cdd91ed8a203b721411de7c9f378813c2d
FilippoPisello <filippo.pisello@live.it>
2025-12-10 19:48:35 +0100

    Works but too slow :(

--- advent_of_code/y2025/day10/filippo/solution.py
+++ advent_of_code/y2025/day10/filippo/solution.py
@@ -50,7 +50,55 @@ def apply_button(state: str, button: tuple[int, ...]) -> str:
 
 
 def main_part_two(problem_input: str) -> Any:
-    return
+    counter = 0
+    for config in problem_input.splitlines():
+        # TODO: joltage as tuple instead of list
+        end_joltage = parse_joltage(config)
+        buttons = parse_buttons(config)
+
+        config_counter = 0
+        # TODO: implement as set
+        states = [[0 for _ in range(len(end_joltage))]]
+        # TODO: replace with broader pareto-inferior logic
+        passed_joltages = states.copy()
+        while end_joltage not in states:
+            new_states: list[list[int]] = []
+            config_counter += 1
+            for state in states:
+                for button in buttons:
+                    new_joltage = apply_button_on_joltage(state, button)
+                    # No point in trying to pass again by a previously
+                    # covered state
+                    if joltage_beyond_target(new_joltage, end_joltage):
+                        continue
+                    if new_joltage in passed_joltages:
+                        continue
+                    # TODO: check if joltage is pareto inferior to current
+                    # voltages
+                    new_states.append(new_joltage)
+                    passed_joltages.append(new_joltage)
+            states = new_states
+
+        counter += config_counter
+
+    return counter
+
+
+def parse_joltage(config: str) -> list[int]:
+    relevant_characters = config[config.index("{") : config.index("}")]
+    simplified_characters = relevant_characters.replace("{", "").replace("}", "")
+    return list(map(int, simplified_characters.split(",")))
+
+
+def apply_button_on_joltage(joltage: list[int], button: tuple[int, ...]) -> list[int]:
+    new_joltage = joltage.copy()
+    for index in button:
+        new_joltage[index] += 1
+    return new_joltage
+
+
+def joltage_beyond_target(joltage: list[int], target: list[int]) -> bool:
+    return any(j > t for j, t in zip(joltage, target))
 
 
 # def main(problem_input: str) -> Any:
--- advent_of_code/y2025/day10/filippo/tests.py
+++ advent_of_code/y2025/day10/filippo/tests.py
@@ -6,9 +6,11 @@ import pytest
 
 from advent_of_code.y2025.day10.filippo.solution import (
     apply_button,
+    apply_button_on_joltage,
     main_part_one,
     main_part_two,
     parse_buttons,
+    parse_joltage,
 )
 
 SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
@@ -56,14 +58,38 @@ def test_cases_part_one(problem_input, expected):
     assert main_part_one(problem_input) == expected
 
 
-@pytest.mark.skip("Remove this line once part two is implemented")
+@pytest.mark.parametrize(
+    ("config", "expected"),
+    (
+        (
+            "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}",
+            [3, 5, 4, 7],
+        ),
+        (
+            "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}",
+            [10, 11, 11, 5, 10, 5],
+        ),
+    ),
+)
+def test_parse_joltage(config, expected):
+    assert parse_joltage(config) == expected
+
+
+@pytest.mark.parametrize(
+    ("state", "button", "expected"),
+    (([0, 1, 2, 3], (1, 3), [0, 2, 2, 4]),),
+)
+def test_apply_buttons_on_joltage(state, button, expected):
+    assert apply_button_on_joltage(state, button) == expected
+
+
 @pytest.mark.parametrize(
     ("problem_input", "expected"),
     (
-        (
-            SAMPLE_INPUT_PATH.read_text(),
-            "expected_result_part_one",
-        ),
+        ("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", 10),
+        # ("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 12),
+        # ("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 11),
+        # (SAMPLE_INPUT_PATH.read_text(), 33),
     ),
 )
 def test_cases_part_two(problem_input, expected):
