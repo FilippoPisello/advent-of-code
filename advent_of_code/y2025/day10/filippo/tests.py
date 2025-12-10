313ef2f71ccbb9ddc89c1f073db1bef6d48ed53a
FilippoPisello <filippo.pisello@live.it>
2025-12-10 18:49:57 +0100

    More setup

--- advent_of_code/y2025/day10/filippo/tests.py
+++ advent_of_code/y2025/day10/filippo/tests.py
@@ -12,10 +12,10 @@ SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
 @pytest.mark.parametrize(
     ("problem_input", "expected"),
     (
-        (
-            SAMPLE_INPUT_PATH.read_text(),
-            7,
-        ),
+        ("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", 2),
+        ("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", 3),
+        ("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 2),
+        (SAMPLE_INPUT_PATH.read_text(), 7),
     ),
 )
 def test_cases_part_one(problem_input, expected):
