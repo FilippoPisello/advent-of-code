0d06beac475aa971324226c1696669a7bf0bf79c
Maxime Bizet <91260999+MaximeBizet@users.noreply.github.com>
2025-12-18 18:20:50 +0100

    Solution day 10, Max (#85)
    * Solution day 10, Max
    
    * Update lock file

---
+++ advent_of_code/y2025/day10/max/tests.py
@@ -0,0 +1,36 @@
+"""Tests for day 10 of Advent of Code 2025, by max."""
+
+from pathlib import Path
+
+import pytest
+
+from advent_of_code.y2025.day10.max.solution import main_part_one, main_part_two
+
+SAMPLE_INPUT_PATH = Path(__file__).resolve().parent / "sample_input.txt"
+
+
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            "expected_result_part_one",
+        ),
+    ),
+)
+def test_cases_part_one(problem_input, expected):
+    assert main_part_one(problem_input) == expected
+
+
+@pytest.mark.skip("Remove this line once part two is implemented")
+@pytest.mark.parametrize(
+    ("problem_input", "expected"),
+    (
+        (
+            SAMPLE_INPUT_PATH.read_text(),
+            "expected_result_part_one",
+        ),
+    ),
+)
+def test_cases_part_two(problem_input, expected):
+    assert main_part_two(problem_input) == expected
